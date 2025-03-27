3169a35f947a8b9fcc890cbdd0bb7bfc1aaa2e59
Matin Dehghanian <matindehghanian98@gmail.com>
2025-03-27 14:31:37 +0330

    Add Helmet and Toaster components for improved SEO and UI notifications

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import "./index.css";
 import App from "./App.tsx";
 import "@/lib/i18n.js";
 import { ThemeProvider } from "@/components/theme/theme-provider.tsx";
+import { Toaster } from "@/components/ui/sonner.tsx";
+import { Helmet } from "react-helmet";
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-    <App />
-  </ThemeProvider>
+  <>
+    <Helmet>
+      <title>{window.location.pathname.split("/")[2]} Sub Stats</title>
+      <meta
+        name="description"
+        content="Powered by https://github.com/MatinDehghanian"
+      />
+    </Helmet>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
+  </>
 );
